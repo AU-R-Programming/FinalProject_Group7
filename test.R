629b2a836115db23b40188b93b971af3233cdae1
Carlos Olivos <cro0010@auburn.edu>
2021-12-01 14:53:42 -0600

    Package implemented. Documentation and validation need to be updated.

---
+++ test.R
@@ -0,0 +1,14 @@
+# # Import data
+abalone <- read.csv(file = 'abalone.csv')
+# 
+# Transform dummy variables
+library(fastDummies)
+abalone <- dummy_cols(abalone, select_columns = "Sex")
+# 
+# Create response variable and set of predictors
+response <- abalone$Rings
+predictor <- abalone[, 2:11]
+predictor <- subset(predictor, select = -Rings)
+
+library(group7project)
+lin_reg(response, predictor, iterations = 20000)
